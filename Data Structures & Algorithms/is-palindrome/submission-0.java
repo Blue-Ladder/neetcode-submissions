0394fba611f5e302aa9f42febb85e2a1ae989f80
차민수 <66284303+Blue-Ladder@users.noreply.github.com>
2026-07-31 16:35:39 +0900

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        
+        while (i < j) {
+            char charI = s.charAt(i);
+            char charJ = s.charAt(j);
+            
+            if (!AlphabetOrNum(charI)) {
+                i++;
+            } 
+            else if (!AlphabetOrNum(charJ)) {
+                j--;
+            } 
+            else {
+                if (Character.toLowerCase(charI) != Character.toLowerCase(charJ)) return false;
+                i++; j--;
+            }
+        }
+        return true;
+    }
+    
+    // Your helper method works perfectly! 
+    public boolean AlphabetOrNum(char c) {
+        return (c >= 'A' && c <= 'Z' ||
+                c >= 'a' && c <= 'z' ||
+                c >= '0' && c <= '9' );
+    }
+}
